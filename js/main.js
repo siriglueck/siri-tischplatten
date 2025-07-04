bbe228d150bc40a3b7e0ba744a472f4e388d804f
siriluck.rattanapong@gmail.com <siriluck.rattanapong@gmail.com>
2025-07-04 11:31:35 +0200

    fix Farbe and add Finish elements

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
+/* === DOM Zugriff & Variablen 1/2 === */
 const form1 = document.getElementById("form1");
 const Breite = document.getElementById("Breite");
 const Laenge = document.getElementById("Laenge");
 const Staerke = document.getElementById("Staerke");
 const tischForm = document.getElementById("tischForm");
 const displayQM = document.getElementById("displayQM");
+const resetButton = document.getElementById("resetBtn");
 const displayPreis = document.getElementById("displayPreis");
 const vorschauList = document.getElementById("vorschau");
 const balkenCheckbox = document.getElementById("Balken");
 const risseCheckbox = document.getElementById("Risse");
-const resetButton = document.getElementById("resetBtn");
 const tableContainer = document.getElementById("tableContainer");
 
 const tischFormList = [
@@ -38,61 +39,44 @@ const preisListe = [
   { key: 120, value: 1040 },
 ];
 
+/* === Rechnen geklickt werden === */
 form1.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent the page from refreshing
+  // Verhindern, dass die Seite aktualisiert wird
+  event.preventDefault(); 
+  
+  /* === DOM Zugriff & Variablen 2/2 === */
   const inputBreite = Breite.value;
   const inputLaenge = Laenge.value;
   const selectedStaerke = Staerke.value;
   const tischItem = tischFormList.find((item) => item.key === tischForm.value);
-  const sPreis = preisListe.find(
-    (item) => item.key === parseInt(selectedStaerke)
-  );
+  const sPreis = preisListe.find((item) => item.key === parseInt(selectedStaerke));
 
   const Rissanteil = document.querySelector('input[name="Rissanteil"]:checked');
   const selectedRissanteil = Rissanteil.value;
 
-  const Farbe = document.querySelector('input[name="Farbe"]:checked');
-  const selectedFarbe = Farbe.value;
+  const Finish = document.querySelector('input[name="Finish"]:checked');
+  const selectedFinish = Finish.value;
 
+  /* === Rechnungen === */
   const qm = (inputBreite / 100) * (inputLaenge / 100);
   const rissePreis = qm * 47.6;
   const balkenPreis = qm * 71.4;
 
   let preisBerechnung;
+  if (qm < 1) { preisBerechnung = qm * sPreis.value * 1.05; } 
+  else { preisBerechnung = qm * sPreis.value; }
 
-  if (qm < 1) {
-    preisBerechnung = qm * sPreis.value * 1.05;
-  } else {
-    preisBerechnung = qm * sPreis.value;
-  }
-
+  
   displayPreis.innerText = preisBerechnung.toFixed(2) + " €";
-
-  addListItem("Breite : ", inputBreite, " cm.");
-  addListItem("Länge : ", inputLaenge, " cm.");
-  addListItem("Stärke : ", selectedStaerke, " mm.");
-  addListItem("Tischform : ", tischItem.value, "");
-  addListItem("Rissanteil : ", selectedRissanteil, "");
-  addListItem("Farbe : ", selectedFarbe, "");
-
-  if (balkenCheckbox.checked) {
-    addListItem("* mit Reine Balken Außenseiten", "", "");
-    displayPreis.innerHTML += "<br> + " + balkenPreis + " € zzgl. Balken";
-  }
-
-  if (risseCheckbox.checked) {
-    addListItem("** Äste/Risse schwarz verfüllt", "", "");
-    displayPreis.innerHTML += "<br> + " + rissePreis + " € zzgl. Risse";
-  }
+  displayQM.innerText = qm.toFixed(2);
 
   function addListItem(text, input, einheit) {
     const newListItem = document.createElement("li");
     newListItem.textContent = text + input + einheit;
     vorschauList.appendChild(newListItem);
   }
-
-  displayQM.innerText = qm.toFixed(2);
-
+  
+  /* === zurücksetzen === */
   resetButton.addEventListener("click", function (e) {
     const confirmReset = confirm("Möcten Sie das Formular zurücksetzen?");
     if (!confirmReset) {
@@ -107,7 +91,7 @@ form1.addEventListener("submit", function (event) {
     }
   });
 
-  //* === Preistabelle generieren === */
+  /* === Preistabelle erzeugen === */
   const preisTable = document.createElement("table");
   preisTable.border = "1";
   const thead = document.createElement("thead");
@@ -116,9 +100,9 @@ form1.addEventListener("submit", function (event) {
     "Plattenstärke (mm)",
     "Preis/m²",
     "Preis",
-    "zzgl. Risse verfüllen 47,60 € / m²",
-    "zzgl. Reine Balken Aussenseiten <br> + 71,40 € / m²",
-    "zzgl. Risse verfüllen und Reine Balken Aussenseiten <br> + 47,60 € / m² <br> + 71,40 € / m²",
+    "zzgl. Risse verfüllen",
+    "zzgl. Reine Balken Aussenseiten",
+    "zzgl. Risse verfüllen und Reine Balken Aussenseiten"
   ];
   // Platzhalter für Tabellekörper
   headers.forEach((text) => {
@@ -173,13 +157,26 @@ form1.addEventListener("submit", function (event) {
           console.log(`Kein Zugriff`);
       }
     }
-
     tbody.appendChild(row);
   }
-
   preisTable.appendChild(tbody);
-
   tableContainer.appendChild(preisTable);
+
+  /* === Vorschau sektion === */
+  addListItem("Breite : ", inputBreite, " cm.");
+  addListItem("Länge : ", inputLaenge, " cm.");
+  addListItem("Stärke : ", selectedStaerke, " mm.");
+  addListItem("Tischform : ", tischItem.value, "");
+  addListItem("Rissanteil : ", selectedRissanteil, "");
+  if (balkenCheckbox.checked) {
+    addListItem("mit Reine Balken Außenseiten*", "", "");
+    displayPreis.innerHTML += "<br> + " + balkenPreis + " € zzgl. Balken";
+  }
+  addListItem("Finish : ", selectedFinish, "");
+  if (risseCheckbox.checked) {
+    displayPreis.innerHTML += "<br> + " + rissePreis + " € zzgl. Risse";
+  }
+
 
   submitBtn.disabled = true;
   submitBtn.textContent = "gerechnet";
